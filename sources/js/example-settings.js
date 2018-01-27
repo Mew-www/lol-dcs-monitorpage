0b4b0026745bee9341cddeb6b26a903d5886204e
Mew-www <mew.proxy@hotmail.com>
2018-01-28 00:38:08 +0200

    Install axios and bootstrap-v4, setup example-settings.js (and gitignore settings.js)

---
+++ sources/js/example-settings.js
@@ -0,0 +1,7 @@
+// Rename this file to "settings.js" for it to have effect
+
+const API_ROOT = 'http://127.0.0.1/dcs/';
+
+export default {
+
+}
---
+++ sources/js/subcomponents/app-component.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Settings from "../settings";
+
+export class AppComponent extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        <button className="bootstrapbtn">just checking sass</button>
+      </div>
+    );
+  }
+}
