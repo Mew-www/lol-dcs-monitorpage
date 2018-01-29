793a102d29482e1ca071a05c50a45416faca01da
Mew-www <mew.proxy@hotmail.com>
2018-01-29 20:50:15 +0200

    Display gathered match count per semver

--- sources/js/subcomponents/gathered-matches-component.jsx
+++ sources/js/subcomponents/gathered-matches-component.jsx
@@ -33,6 +33,17 @@ export class GatheredMatchesComponent extends Component {
       }
       return tier_to_num(a) - tier_to_num(b);
     }
+    let spanned_semvers = Object.keys(this.state.matches)
+      .filter(tier => tier!=='total')
+      .reduce((accumulator, tier) => {
+        for (let semver of Object.keys(this.state.matches)) {
+          if (accumulator.indexOf(semver) === -1) {
+            accumulator.push(semver);
+          }
+        }
+        return accumulator;
+      }, [])
+      .sort();
     return (
       <div className="GatheredMatches">
         <div className="GatheredMatches-title">
@@ -42,11 +53,26 @@ export class GatheredMatchesComponent extends Component {
           <div className="GatheredMatches-region" key={region}>
             <table className="GatheredMatches-table">
               <caption className="GatheredMatches-table-caption">{region}</caption>
+              <thead>
+                <tr>
+                  <th> </th>
+                  {spanned_semvers.map((semver) =>
+                    <th key={semver}>{semver}</th>
+                  )}
+                </tr>
+              </thead>
               <tbody>
                 {Object.keys(this.state.matches['per_region'][region]).sort(sort_tiers).map((tier) =>
                   <tr className="GatheredMatches-table-row" key={region+tier}>
                     <td className="GatheredMatches-table-cell">{tier}:</td>
-                    <td className="GatheredMatches-table-cell">{this.state.matches['per_region'][region][tier]}</td>
+                    {spanned_semvers.map((semver) =>
+                      <td className="GatheredMatches-table-cell">{
+                        Object.keys(this.state.matches['per_region'][region][tier]).indexOf(semver) !== -1 ?
+                          this.state.matches['per_region'][region][tier][semver]
+                          :
+                          "0"
+                      }</td>
+                    )}
                   </tr>
                 )}
               </tbody>
